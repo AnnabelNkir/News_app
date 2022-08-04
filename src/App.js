4be0e6527e8155b3a994d3dd5c54965500dd8371
AnnabelNKir <nkiroteannabel@gmail.com>
2022-08-04 21:24:13 +0300

    A simple news app created using react.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import About from './components/About';
+
+
 function App() {
+  let PageSize= 9;
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <NavBar/>
+    <Routes>
+          <Route path="/"  element={<News pageSize={PageSize} country="in" category="general"/>} />
+          <Route path="/about"  element={<About/>} />
+
+          {/* catagory */}
+          <Route exact path="/general" element={<News key="General"  pageSize={PageSize} country="in" category="General"/>} />
+          <Route exact path="/business" element={<News key="Business" pageSize={PageSize} country="in" category="Business"/>} />
+          <Route exact path="/entertainment" element={<News   key="Entertainment" pageSize={PageSize} country="in" category="Entertainment"/>} />
+          <Route exact path="/health" element={<News key="Health" pageSize={PageSize} country="in" category="Health"/>} />
+          <Route exact path="/science"element={<News key="Science" pageSize={PageSize} country="in" category="Science"/>} />
+          <Route exact path="/sports" element={<News key="Sports" pageSize={PageSize} country="in" category="Sports"/>} />
+          <Route exact path="/technology" element={<News key="Technology" pageSize={PageSize} country="in" category="Technology"/>} />
+          
+
+      </Routes> 
+          
+   
+    
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>A simple news app created in React.js. 
+      It shows various categories of news articles; technology, business, sports, entertainment etc. 
+      It uses the news API to generate this.</div>
+    )
+  }
+}

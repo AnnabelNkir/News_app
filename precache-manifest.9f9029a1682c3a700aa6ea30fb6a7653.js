03532250b831921b6b6563f1f3a48bd6c381459c
AnnabelNKir <nkiroteannabel@gmail.com>
2022-08-23 17:55:55 +0300

    Updates

---
+++ precache-manifest.9f9029a1682c3a700aa6ea30fb6a7653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f057d9eca5063c5b3c1b",
+    "url": "/News_app/static/css/main.22b65796.chunk.css"
+  },
+  {
+    "revision": "f057d9eca5063c5b3c1b",
+    "url": "/News_app/static/js/main.f057d9ec.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/News_app/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "9c972fcdc5d33b9c35b3",
+    "url": "/News_app/static/js/2.9c972fcd.chunk.js"
+  },
+  {
+    "revision": "bfaf1c502fc9569cfde7",
+    "url": "/News_app/static/js/runtime~main.bfaf1c50.js"
+  },
+  {
+    "revision": "094c90258a7b38dfb7cef5e06728f5c2",
+    "url": "/News_app/index.html"
+  }
+];
